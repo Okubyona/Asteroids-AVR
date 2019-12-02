25768f0f0063d4a8dfca23d682806ce25880164d
Okubyona <andrewbazua@gmail.com>
2019-12-02 14:11:16 -0800

    Feat: add/confirm joystick functionality
    Changes to adc.h:
        - Set ADC circuit speed to 125KHz via ADSP[2:0] registers (ADCSRA)
            + Include documentation/explanation for what these do.
        - Set ADC interrupts via ADIE register                    (ADCSRA)
        - [NEW!] ISR(ADC_vect), ADC interupt function
            + Switch between the first two ADC channels PINA0 and PINA1.
            + Store 8 most significant bits ADCH in two volatile variables
                to use outside of ISR function.
        # All other registers were left unchanged.
    
    Changes to io.h and io.c: (Will be removed upon 1.0 update)
        - Add includes to io.h; io.c now just includes io.h.
        - Remove obsolete function LCD_DisplayInteger().
    
    Changes to joystickTest.c:
        - Now use sprintf() to convert ADC data from joystick to char format
            to allow easy means of displaying to LCD via LCD_DisplayString.

--- header/io.h
+++ header/io.h
@@ -5,13 +5,11 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "io.h"
 
 void LCD_init();
 void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
 void LCD_WriteData(unsigned char Data);
-void LCD_DisplayInteger(unsigned char column, unsigned char num);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column ,const unsigned char *string);
 void delay_ms(int miliSec);
--- test/joystickTest.c
+++ test/joystickTest.c
@@ -1,31 +1,39 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 #include "adc.h"
 #include "tasks.h"
 #include "timer.h"
 #include "io.h"
 
+typedef enum joystickTick{channel0, channel1};
+
+int joystickTick(int state);
+
 int main(void) {
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
 
+    char adcVal[10];
+
     ADC_init();
     LCD_init();
-
-
     TimerSet(10);
     TimerOn();
 
+
     while (1) {
-        LCD_DisplayString(1, "X: ");
+        //Loads ADC val from channel 0 into adcVal
+        sprintf(adcVal, "X: %d", sensor1);
+        LCD_DisplayString(1, adcVal);
 
-        LCD_DisplayInteger(4, sensor0);
+        sprintf(adcVal, "Y: %d", sensor0);
+        LCD_DisplayString(17, adcVal);
 
-        LCD_DisplayString(17, "Y: ");
 
-        LCD_DisplayInteger(20, sensor1);
-
+        while (!TimerFlag);
+        TimerFlag = 0;
     }
 
     return 0;
