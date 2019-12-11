401c2d666c88ef6826a387588bb1cf0b647349fd
Okubyona <andrewbazua@gmail.com>
2019-12-11 02:15:55 -0800

    Fix: add correct/functioning code for menus

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #include <avr/eeprom.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "adc.h"
 #include "tasks.h"
 #include "timer.h"
@@ -30,11 +31,12 @@ static unsigned char activeLaser = 0; // only one laser can be on the screen at 
 static unsigned char asteroidHit = 0; // 0-4 where 1 = small 2 = medium 3 = large and 0 = no hit;
 static unsigned char resetFlag = 0;  //  tells menu/ game to reset game/ highscore.
 static unsigned char menuEnd = 0;	// menu end for gameStart
-static unsigned char gameEnd = 0; // lets gameSM know when to run.
+static unsigned char gameStart = 0; // lets gameSM know when to run.
+static unsigned char gameEnd = 0;
 
-uint16 EEMEM ee_highscore_easy = 0;
+uint16_t EEMEM ee_highscore_easy;
 
-uint16 highscore;
+uint16_t highscore;
 unsigned short score;
 
 int main(void) {
@@ -44,7 +46,7 @@ int main(void) {
 	DDRD = 0xF0; PORTD = 0x0F;
 
 	LCD_BuildCustomChars();
-	highscore = eeprom_read_word(&ee_highscore);
+	highscore = eeprom_read_word(&ee_highscore_easy);
 
 	unsigned char i;
 	static task task1, task2, task3, task4, task5;
@@ -66,15 +68,15 @@ int main(void) {
 	task3.elapsedTime = task3.period;
 	task3.TickFct = &resetTick;
 
-    task4.state = m_title;
-    task4.period = 50;
-    task4.elapsedTime = task4.period;
-    task4.TickFct = &menuTick;
+	task4.state = m_title;
+	task4.period = 50;
+	task4.elapsedTime = task4.period;
+	task4.TickFct = &menuTick;
 
-    task5.state = g_wait;
-    task5.period = 100;
-    task5.elapsedTime = task5.period;
-    task5.TickFct = &gameTick;
+	task5.state = g_wait;
+	task5.period = 100;
+	task5.elapsedTime = task5.period;
+	task5.TickFct = &gameTick;
 
 
 	unsigned long GCD = tasks[0]->period;
@@ -108,22 +110,20 @@ int joystickTick(int state) {
 
 	switch (state) {
 		case j_wait:
-    		if (sensor1 < 120) { state = j_left; }
-    		else if (sensor1 > 128) { state = j_right; }
-    		else { state = j_wait; }
-    		break;
+		if (sensor1 < 120) { state = j_left; }
+		else if (sensor1 > 128) { state = j_right; }
+		else { state = j_wait; }
+		break;
 
 		case j_left:
-    		if (sensor1 < 120) { state = j_left; }
-    		else { state = j_wait; }
-    		break;
+		if (sensor1 < 120) { state = j_left; }
+		else { state = j_wait; }
+		break;
 
 		case j_right:
-    		if (sensor1 > 128) { state = j_right; }
-    		else {state = j_wait; }
-    		break;
-
-        case j_up:
+		if (sensor1 > 128) { state = j_right; }
+		else {state = j_wait; }
+		break;
 
 	}
 
@@ -143,36 +143,36 @@ int laserTick(int state) {
 
 	switch (state) {
 		case b_wait:
-	       state = A3 ? b_press: b_wait;
-           break;
+		state = A3 ? b_press: b_wait;
+		break;
 
 		case b_press:
-	       state = b_cooldown;
-           break;
+		state = b_cooldown;
+		break;
 
 		case b_cooldown:
-	       state = A3 ? b_cooldown: b_wait;
-           break;
+		state = A3 ? b_cooldown: b_wait;
+		break;
 	}
 
 	switch (state) {
 		case b_wait: laserFired = 0; break;
 
 		case b_press:
-            if (gameStart) {
-                laserFired = activeLaser ? 0: 1;
-            }
-            else {
-                laserFired = A3 ? 1: 0;
-            }
-            // laser will only be fired when there is no laser character on the
-            // screen.
-            break;
+		if (gameStart) {
+			laserFired = activeLaser ? 0: 1;
+		}
+		else {
+			laserFired = A3 ? 1: 0;
+		}
+		// laser will only be fired when there is no laser character on the
+		// screen.
+		break;
 
 		case b_cooldown:
-		   state = A3 ? b_cooldown: b_wait;
-           laserFired = 0;
-	       break;
+		state = A3 ? b_cooldown: b_wait;
+		laserFired = 0;
+		break;
 
 	}
 
@@ -198,12 +198,15 @@ int resetTick(int state) {
 	}
 
 	switch (state) {
-		case r_wait: break;
+		case r_wait:
+			resetFlag = 0;
+			break;
 
 		case r_press:
-		eeprom_write_word(&ee_highscore, 0);
-        highscore = ee_highscore;
-		break;
+			eeprom_write_word(&ee_highscore_easy, 0);
+			highscore = &ee_highscore_easy;
+			resetFlag = 1;
+			break;
 
 		case r_hold: break;
 	}
@@ -217,37 +220,39 @@ int menuTick(int state) {
 	static char buffer0[17];
 	static char buffer1[17];
 
-    switch (state) {
-        case m_title:
-            state = laserFired ? m_difficulty_select: m_title;
-            break;
+	switch (state) {
+		case m_title:
+			state = laserFired ? m_difficulty_select: m_title;
+			break;
 
 		case m_difficulty_select:
 			state = laserFired ? m_highscore: m_difficulty_select;
+			if (resetFlag) { state = m_title; }
 			break;
 
 		case m_highscore:
 			state = laserFired ? m_idle: m_highscore;
+			if (resetFlag) { state = m_title; }
 			break;
 
 		case m_idle:
 			state = gameEnd ? m_title: m_idle;
+			if (resetFlag) { state = m_title; }
 			break;
-    }
+	}
 
-    switch (state) {
+	switch (state) {
 		case m_title:
 			menuEnd = 0;
 			sprintf(buffer0, "Asteroids Puzzle");
 			if (strcmp(buffer0, check0) != 0) { // if strings aren't equal
-				LCD_DisplayString(1, buffer);
-				check0 = buffer0;
+			LCD_DisplayString(1, buffer0);
+			strcpy(check0, buffer0);
 			}
-
-			sprintf(buffer, "                ");
+			sprintf(buffer1, "                ");
 			if (strcmp(buffer1, check1) != 0) {
-				LCD_DisplayString(17, buffer);
-				check1 = buffer1;
+			LCD_DisplayString(17, buffer1);
+			strcpy(check1, buffer1);
 			}
 			break;
 
@@ -255,34 +260,43 @@ int menuTick(int state) {
 			sprintf(buffer0, "   Difficulty   ");
 			if (strcmp(buffer0, check0) != 0) {
 				LCD_DisplayString(1, buffer0);
-				check0 = buffer0;
+				strcpy(check0, buffer0);
 			}
 			sprintf(buffer1, "      Easy      ");
 			if (strcmp(buffer1, check1) != 0) {
-				LCD_DisplayString(1, buffer1);
-				check1 = buffer1;
+				LCD_DisplayString(17, buffer1);
+				strcpy(check1, buffer1);
+				LCD_Cursor(17 + 4);
 			}
 			break;
 
-		case m_highscore;
+		case m_highscore:
 			sprintf(buffer0, "   High Score   ");
 			if (strcmp(buffer0, check0) != 0) {
 				LCD_DisplayString(1, buffer0);
-				check0 = buffer0;
+				strcpy(check0, buffer0);
 			}
-			sprintf(buffer1, "      %d", highscore);
+			sprintf(buffer1, "       %d", highscore);
+			if (strcmp(buffer1, check1) != 0) {
+				LCD_DisplayString(17, buffer1);
+				LCD_WriteData(' ');
+				LCD_WriteData(' ');
+				LCD_WriteData(' ');
+				LCD_Cursor(0);
+				strcpy(check1, buffer1);
+			}
 			break;
 
 		case m_idle:
 			menuEnd = 1;
 			break;
 
-    }
+	}
 
-    return state;
+	return state;
 }
 
 int gameTick(int state) {
 
-    return state;
+	return state;
 }
